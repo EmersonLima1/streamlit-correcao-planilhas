871177a6bd9d096e74506b22eccb2cfb50d23448
EmersonLima1 <74200172+EmersonLima1@users.noreply.github.com>
2023-05-15 20:04:43 -0300

    Update app_planilhas.py

--- app_planilhas.py
+++ app_planilhas.py
@@ -2,16 +2,33 @@ import pandas as pd
 import streamlit as st
 from io import BytesIO
 
-
 def remove_duplicated_rows(df):
     df.drop_duplicates(inplace=True)
     return df
-
 
 def remove_blank_rows(df):
     df.dropna(inplace=True)
     return df
 
+def convert_to_complete_date(df, column_name):
+    df[column_name] = pd.to_datetime(df[column_name])
+    return df
+
+def create_day_column(df, column_name):
+    df[column_name + "_day"] = df[column_name].dt.day
+    return df
+
+def create_month_column(df, column_name):
+    df[column_name + "_month"] = df[column_name].dt.month
+    return df
+
+def create_year_column(df, column_name):
+    df[column_name + "_year"] = df[column_name].dt.year
+    return df
+
+def capitalize_first_letter(df, column_name):
+    df[column_name] = df[column_name].str.title()
+    return df
 
 def main():
     st.title("Correções em Arquivos Excel")
@@ -24,18 +41,40 @@ def main():
         st.subheader("DataFrame Original")
         st.write(df)
 
-        st.subheader("Deseja realizar quais correções?")
-        corrections = st.multiselect(
-            "Selecione as correções desejadas:",
-            ["Remover linhas duplicadas", "Remover linhas em branco"]
-        )
+        column_names = list(df.columns)
+        selected_column = st.selectbox("Selecione a coluna para correção", column_names)
 
-        if st.button("Aplicar correções"):
-            if "Remover linhas duplicadas" in corrections:
-                df = remove_duplicated_rows(df)
+        if selected_column:
+            selected_column_type = df[selected_column].dtype
 
-            if "Remover linhas em branco" in corrections:
-                df = remove_blank_rows(df)
+            st.subheader(f"Correções para a coluna '{selected_column}'")
+
+            if selected_column_type == "datetime64[ns]":
+                date_corrections = st.multiselect(
+                    "Selecione as correções desejadas:",
+                    ["Converter para data completa", "Criar coluna de dia", "Criar coluna de mês", "Criar coluna de ano"]
+                )
+
+                if "Converter para data completa" in date_corrections:
+                    df = convert_to_complete_date(df, selected_column)
+
+                if "Criar coluna de dia" in date_corrections:
+                    df = create_day_column(df, selected_column)
+
+                if "Criar coluna de mês" in date_corrections:
+                    df = create_month_column(df, selected_column)
+
+                if "Criar coluna de ano" in date_corrections:
+                    df = create_year_column(df, selected_column)
+
+            elif selected_column_type == "object":
+                text_corrections = st.multiselect(
+                    "Selecione as correções desejadas:",
+                    ["Converter primeira letra de cada palavra para maiúscula"]
+                )
+
+                if "Converter primeira letra de cada palavra para maiúscula" in text_corrections:
+                    df = capitalize_first_letter(df, selected_column)
 
             st.subheader("DataFrame Corrigido")
             st.write(df)
@@ -52,7 +91,6 @@ def main():
                 file_name="dataframe_corrigido.xlsx",
                 mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
             )
-
 
 if __name__ == "__main__":
     main()
